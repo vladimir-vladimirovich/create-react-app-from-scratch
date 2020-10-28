6fec13bba100c986fbdaaa9481cf89265413599a
Vladimir Ovcharov <vladimir.ovcharov@playtech.com>
2020-10-28 17:26:49 +0200

    inint

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
